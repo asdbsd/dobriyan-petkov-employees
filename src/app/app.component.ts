fc7a25d0caed01f42b9acba5020fce98b75a0d75
Dobriyan Petkov <dobriyanp@yahoo.com>
2023-02-16 10:46:21 +0000

    updated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dobriyan-petkov-employees';
+
+  rowsArray: string[] | undefined = undefined;
+
+  onFileSelected(event: any): void {
+
+    if (event.srcElement.files.length > 1 || event.srcElement.files[0].size > 5000) {
+      console.log('Wrong Input, Please try again.');
+      return;
+    }
+
+    const file: Blob = event.srcElement.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      let csvData = reader.result as string;
+      let csvRecordsArray = (<string>csvData).split(/\r\n|\n/).filter((string) => !string.split(',').includes(''));
+      csvRecordsArray.shift();
+      this.rowsArray = csvRecordsArray;
+    };
+    reader.onload(event);
+
+  }
 }
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-upload',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent implements OnInit {
+  @Input() rowsArray!: string[];
+
+
+  rows: string[] | undefined = undefined;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+
+
+
+
+
 
 }
